ddd0c78b28c2058aa0320e1a18e8a66b2b68685e
Li ZHOU <lizhou@localhost.localdomain>
2017-03-28 15:25:28 +0800

    2nd commit

---
+++ build/CMakeFiles/ssclient.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/cipher.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/cipher.c.o"
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/client.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/client.c.o"
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/main.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/main.c.o"
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/s5.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/s5.c.o"
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/server.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/server.c.o"
+  "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/util.c" "/home/lizhou/dist.libuv.org/dist/v1.11.0/libuv-v1.11.0/samples/mysocks/build/CMakeFiles/ssclient.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/debug/CMakeFiles/ssclient.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ssclient.dir/client.c.o"
+  "CMakeFiles/ssclient.dir/cipher.c.o"
+  "CMakeFiles/ssclient.dir/main.c.o"
+  "CMakeFiles/ssclient.dir/s5.c.o"
+  "CMakeFiles/ssclient.dir/server.c.o"
+  "CMakeFiles/ssclient.dir/util.c.o"
+  "bin/ssclient.pdb"
+  "bin/ssclient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ssclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
