af7b4c0a16135113dc3186f102bed4ab18d911e5
U-LiZHOU-PC\Li ZHOU <lzh@cpan.org>
2017-04-11 09:25:14 +0800

    fix build with VC

--- src/cipher.c
+++ src/cipher.c
@@ -79,7 +79,7 @@ unsigned char * cipher_encrypt(conn* c, size_t * encryptl,
         //        RAND_bytes(cipher.encrypt.iv.base, cipher.encrypt.iv.len);
 
 #ifdef _MSC_VER 
-        msc_getentropy(cipher.encrypt.iv.base);
+        msc_getentropy(cipher.encrypt.iv);
 #endif
 #if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
         /* UNIX-style OS. ------------------------------------------- */
