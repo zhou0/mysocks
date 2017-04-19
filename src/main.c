6069bc24756d4dec652af99494d662a74dd3672e
Li ZHOU <lzh@cpan.org>
2017-04-19 16:42:44 +0800

    bump version to 0.5.0

--- src/main.c
+++ src/main.c
@@ -135,7 +135,7 @@ static void usage(void)
 {
 
     printf("\n"
-           "This is MySocks project, the lightweight multi-platform shadowsocks client and server,version 0.4, subversion 2 (v0.4.2). Copyright 2017, Li ZHOU\n"
+           "This is MySocks project, the lightweight multi-platform shadowsocks client and server,version 0.5, subversion 0 (v0.5.0). Copyright 2017, Li ZHOU\n"
            "\n"
            "Usage:%s [-b <local_host>] [-h] -k <password> -m <encrypt_method> [-l <port>] -p <remote_port> -s <remote_host>\n"
            "\n"
