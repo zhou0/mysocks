16e924a120c72746ad087af1f2cc054101c89b32
U-LiZHOU-PC\Li ZHOU <lzh@cpan.org>
2017-04-22 12:48:32 +0800

    fix a compile warning

--- src/client.h
+++ src/client.h
@@ -14,37 +14,37 @@ extern "C" {
 
 #include "defs.h"
   
-static void do_next(client_ctx *cx);
-static int do_handshake(client_ctx *cx);
-static int do_handshake_auth(client_ctx *cx);
-static int do_req_start(client_ctx *cx);
-static int do_req_parse(client_ctx *cx);
-static int do_req_lookup(client_ctx *cx);
-static int do_req_connect_start(client_ctx *cx);
-static int do_req_connect(client_ctx *cx);
-static int do_proxy_start(client_ctx *cx);
-static int do_proxy(client_ctx *cx);
+ void do_next(client_ctx *cx);
+ int do_handshake(client_ctx *cx);
+ int do_handshake_auth(client_ctx *cx);
+ int do_req_start(client_ctx *cx);
+ int do_req_parse(client_ctx *cx);
+ int do_req_lookup(client_ctx *cx);
+ int do_req_connect_start(client_ctx *cx);
+ int do_req_connect(client_ctx *cx);
+ int do_proxy_start(client_ctx *cx);
+ int do_proxy(client_ctx *cx);
 int do_kill(client_ctx *cx);
-static int do_almost_dead(client_ctx *cx);
-static int conn_cycle(const char *who, conn *a, conn *b);
-static void conn_timer_reset(conn *c);
-//static void conn_timer_expire(uv_timer_t *handle, int status);
-static void conn_timer_expire(uv_timer_t *handle);
-static void conn_getaddrinfo(conn *c, const char *hostname);
-static void conn_getaddrinfo_done(uv_getaddrinfo_t *req,
+ int do_almost_dead(client_ctx *cx);
+ int conn_cycle(const char *who, conn *a, conn *b);
+ void conn_timer_reset(conn *c);
+// void conn_timer_expire(uv_timer_t *handle, int status);
+ void conn_timer_expire(uv_timer_t *handle);
+ void conn_getaddrinfo(conn *c, const char *hostname);
+ void conn_getaddrinfo_done(uv_getaddrinfo_t *req,
                                   int status,
                                   struct addrinfo *ai);
-static int conn_connect(conn *c);
-static void conn_connect_done(uv_connect_t *req, int status);
-static void conn_read(conn *c);
-static void conn_read_done(uv_stream_t *handle,
+ int conn_connect(conn *c);
+ void conn_connect_done(uv_connect_t *req, int status);
+ void conn_read(conn *c);
+ void conn_read_done(uv_stream_t *handle,
                            ssize_t nread,
                            const uv_buf_t *buf);
-static void conn_alloc(uv_handle_t *handle, size_t size, uv_buf_t *buf);
-static void conn_write(conn *c, const void *data, unsigned int len);
-static void conn_write_done(uv_write_t *req, int status);
-static void conn_close(conn *c);
-static void conn_close_done(uv_handle_t *handle);
+ void conn_alloc(uv_handle_t *handle, size_t size, uv_buf_t *buf);
+ void conn_write(conn *c, const void *data, unsigned int len);
+ void conn_write_done(uv_write_t *req, int status);
+ void conn_close(conn *c);
+ void conn_close_done(uv_handle_t *handle);
 
 
 #ifdef	__cplusplus
